f3ba2b1b32163e29219300e0654cb1ede066ac8e
Yuta Kanayama <kanayama.work@gmail.com>
2014-12-12 11:57:23 +0900

    style.cssとreset.cssを分けました。cssを圧縮しないようにしました。

--- config.rb
+++ config.rb
@@ -4,6 +4,6 @@ sass_dir = "htdocs/scss"
 images_dir = "htdocs/img"
 javascripts_dir = "htdocs/js"
 
-output_style = :compressed
-#output_style = :expanded
+#output_style = :compressed
+output_style = :expanded
 line_comments = false
